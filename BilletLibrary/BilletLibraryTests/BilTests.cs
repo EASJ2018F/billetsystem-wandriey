f39a8dff7cad9be5ffca8bd22e602d1331c73482
wandriey <jona434g@edu.easj.dk>
2018-01-30 14:31:12 +0100

    Bil class oprettet samt tilsvarende UnitTest
    Bil klasse med 2 propperties og 2 metoder oprettet.

---
+++ BilletLibrary/BilletLibraryTests/BilTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BilletLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilletLibrary.Tests
+{
+    [TestClass()]
+    public class BilTests
+    {
+        [TestMethod()]
+        public void prisTest()
+        {
+            //arrange 
+            var bil = new Bil();
+
+            //act
+            int bilPris = bil.pris();
+
+            //Assert
+            Assert.AreEqual(25, bilPris);
+        }
+
+        [TestMethod()]
+        public void køretøjTest()
+        {
+            //arrange 
+            var bil = new Bil();
+
+            //act
+            string køretøj = bil.køreTøj();
+
+            //Assert
+            Assert.AreEqual("bil", køretøj);
+        }
+    }
+}
